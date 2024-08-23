ca0202ef91f186ba5e454c4ce3bb626961108669
Ihor <ihor.bondarenko@infologiq.com>
2024-08-23 13:26:08 +0300

    feat: update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,13 +5,33 @@ class HomePage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _HomePageState();
-
 }
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+      ),
+      body: _buildUI(context),
+    );
   }
+}
 
+Widget _buildUI(BuildContext context) {
+  return SafeArea(
+      child: SingleChildScrollView(
+    child: Container(
+      width: MediaQuery.sizeOf(context).width,
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.02,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    ),
+  ));
 }
