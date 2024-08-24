08ca5475d9b91187a5fccf2699f7bd5d4007591f
Ihor <ihor.bondarenko@infologiq.com>
2024-08-24 17:05:27 +0300

    feat: create database service

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pokemon_app/pages/home_page.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pokemon_app/services/database_service.dart';
 import 'package:pokemon_app/services/http_service.dart';
 
 Future<void> _initServices() async {
   GetIt.instance.registerSingleton<HttpService>(HttpService());
+  GetIt.instance.registerSingleton<DatabaseService>(DatabaseService());
 }
 
 Future<void> main() async {
