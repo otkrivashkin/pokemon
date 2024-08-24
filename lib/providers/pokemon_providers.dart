8696f46e43fde255a1e7cea3641510c688e560b1
Ihor <ihor.bondarenko@infologiq.com>
2024-08-24 17:13:54 +0300

    feat: persist favorite pokemons

--- lib/providers/pokemon_providers.dart
+++ lib/providers/pokemon_providers.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pokemon_app/models/pokemon/pokemon.dart';
+import 'package:pokemon_app/services/database_service.dart';
 import 'package:pokemon_app/services/http_service.dart';
 
 final pokemonDataProvider =
@@ -20,17 +21,27 @@ final favoritePokemonsProvider =
 });
 
 class FavoritePokemonsProvider extends StateNotifier<List<String>> {
+  final DatabaseService _databaseService =
+      GetIt.instance.get<DatabaseService>();
+
+  String FAVORITE_POKEMON_LIST_KEY = "favorite_pokemon_list";
+
   FavoritePokemonsProvider(super.state) {
     _setup();
   }
 
-  Future<void> _setup() async {}
+  Future<void> _setup() async {
+    List<String>? res = await _databaseService.getList(FAVORITE_POKEMON_LIST_KEY);
+    state = res ?? [];
+  }
 
   void addFavoritePokemon(String url) {
     state = [...state, url];
+    _databaseService.saveList(FAVORITE_POKEMON_LIST_KEY, state);
   }
 
   void removeFavoritePokemon(String url) {
     state = state.where((url) => url != url).toList();
+    _databaseService.saveList(FAVORITE_POKEMON_LIST_KEY, state);
   }
 }
